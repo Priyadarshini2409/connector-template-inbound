c4253317a0ee493fca83eef60e89dd85b5dd16f6
Priyadarshini2409 <sit19ec091@sairamtap.edu.in>
2024-07-17 12:59:39 +0530

    Update MyConnectorExecutable.java

--- src/main/java/io/camunda/connector/inbound/MyConnectorExecutable.java
+++ src/main/java/io/camunda/connector/inbound/MyConnectorExecutable.java
@@ -1,27 +1,47 @@
 package io.camunda.connector.inbound;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import io.camunda.connector.api.annotation.InboundConnector;
 import io.camunda.connector.api.inbound.InboundConnectorContext;
 import io.camunda.connector.api.inbound.InboundConnectorExecutable;
-import io.camunda.connector.inbound.subscription.MockSubscription;
-import io.camunda.connector.inbound.subscription.MockSubscriptionEvent;
+import io.camunda.connector.inbound.subscription.WatchServiceSubscription;
+import io.camunda.connector.inbound.subscription.WatchServiceSubscriptionEvent;
 
-@InboundConnector(name = "My Inbound Connector", type = "io.camunda:my-inbound-connector:1")
+@InboundConnector(
+        name = "Watch Service Inbound Connector", 
+        type = "io.camunda:watchserviceinbound:1"
+    )  
 public class MyConnectorExecutable implements InboundConnectorExecutable<InboundConnectorContext> {
 
-  private MockSubscription subscription;
-
+  private WatchServiceSubscription subscription;
   private InboundConnectorContext context;
+  private ExecutorService executorService;
+  public CompletableFuture<?> future;
 
   @Override
-  public void activate(InboundConnectorContext connectorContext) {
-    this.context = connectorContext;
-    var props = connectorContext.bindProperties(MyConnectorProperties.class);
-    subscription = new MockSubscription(props.sender(), props.messagesPerMinute(), this::onEvent);
+  public void activate(InboundConnectorContext context) {
+    MyConnectorProperties props = context.bindProperties(MyConnectorProperties.class);
+    this.context = context;
+    this.executorService = Executors.newSingleThreadExecutor();
+
+    this.future =
+      CompletableFuture.runAsync(
+        () -> {
+          new WatchServiceSubscription(
+            props.eventToMonitor(), 
+            props.directory(), 
+            props.pollingInterval(), 
+            this::onEvent);
+        },
+        this.executorService);
   }
 
-  private void onEvent(MockSubscriptionEvent rawEvent) {
-    context.correlate(new MyConnectorEvent(rawEvent));
+  private void onEvent(WatchServiceSubscriptionEvent rawEvent) {
+    MyConnectorEvent connectorEvent = new MyConnectorEvent(rawEvent);
+    context.correlateWithResult(connectorEvent);
   }
 
   @Override
